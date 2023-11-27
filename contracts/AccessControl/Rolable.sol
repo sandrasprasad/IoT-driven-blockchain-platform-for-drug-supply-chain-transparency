4a9731f55a917059ddbbf54f8fa31e5413f43ae1
aswinr <aswinr019@gmail.com>
2023-11-27 23:03:47 +0530

    Directory setup

---
+++ contracts/AccessControl/Rolable.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/// Import all Roles
+import "./Roles/Consumer.sol";
+import "./Roles/Designer.sol";
+import "./Roles/Distributor.sol";
+import "./Roles/Manufacturer.sol";
+import "./Roles/Regulator.sol";
+import "./Roles/Retailer.sol";
