16d112fecd6ce0c57e657e724f8fb5556e9ddde8
aswinr <aswinr019@gmail.com>
2023-12-07 06:28:18 +0530

    Added new depencies , changed compiler version in truffle and updated compiler version in contracts

--- contracts/AccessControl/Rolable.sol
+++ contracts/AccessControl/Rolable.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.9;
 
 /// Import all Roles
 import "./Roles/Consumer.sol";
@@ -9,4 +9,30 @@ import "./Roles/Distributor.sol";
 import "./Roles/Manufacturer.sol";
 import "./Roles/Regulator.sol";
 import "./Roles/Retailer.sol";
-  
+
+/// @title Role Ability Contract
+/// @dev contract for adopting all roles in access control
+
+contract Rolable is Consumer, Designer, Distributor, Regulator, Retailer {
+    /// Function to check all roles for the caller
+    /// @return array contining every role with its state as boolean
+    function whoAmI()
+        public
+        view
+        returns (
+            bool consumer,
+            bool retailer,
+            bool distributor,
+            bool manufacturer,
+            bool regulator,
+            bool designer
+        )
+    {
+        consumer = amIConsumer();
+        retailer = amIRetailer();
+        distributor = amIDistributor();
+        manufacturer = amIManufacturer();
+        regulator = amIRegulator();
+        designer = amIDesigner();
+    }
+}
